5993ef137eb4ad16017d6bb649db79238a1f6d3a
Tolu Oyeniyi <tjoye20@gmail.com>
2016-10-22 14:44:47 -0500

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :reviews
+
+  resources :reviews, only: [:show] do
+    collection do
+      post :search
+    end
+  end 
+
+  resources :businesses, only: [:create]
+
   root "reviews#new"
 end
