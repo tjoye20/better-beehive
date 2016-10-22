bc23730ebc53f745e28a2c017484592fa2f518e6
Tolu Oyeniyi <tjoye20@gmail.com>
2016-10-22 16:10:04 -0500

    Fix json render.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,14 +7,18 @@ class ReviewsController < ApplicationController
 
   def show
     @business = Business.find(params[:id])
+    binding.pry
     @reviews = @business.reviews
 
-    format.json {
-      render :json => {
-      :business => @business,
-      :reviews => @reviews
-      }
-    }
+    respond_to do |format|
+      format.js do
+        render :json => {
+          :business => @business,
+          :reviews => @reviews
+        }
+      end
+    end
+    
   end
 
 end
