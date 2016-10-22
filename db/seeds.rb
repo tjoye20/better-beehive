50a77a6010fa96a87d940f31ba4ecfabf327f80f
Jeff Pape <jeff.pape@gmail.com>
2016-10-22 14:41:21 -0500

    Added a 1 social and 2 medical businesses to the seeds database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,16 @@
 
 Business.create(name: "Waterloo Medical Center", type: "", buzzes: 10, phone_number: "555-555-5555",
                 special_events: "", address: "260 Sheridan Ave., Suite B20", zip_code: "94306")
+
+Business.create(name: "Waterloo Ice House", business_type: "social", buzzes: 20, stings: 3, phone_number: "512-418-9700",
+                special_events: "", address: "6203 N Capital of Texas Hwy.", zip_code: "78731")
+
+Business.create(name: "Grim Reaper", business_type: "medical", buzzes: 2, stings: 30, phone_number: "737-666-5014",
+                special_events: "", address: "1000 Graveyard Way", zip_code: "78456")
+
+Business.create(name: "Mayo Clinic", business_type: "medical", buzzes: 43, stings: 7, phone_number: "737-777-8000",
+                special_events: "", address: "2000 W 29th St.", zip_code: "78394")
+
+
+
+
