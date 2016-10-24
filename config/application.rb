9dc53a77676b7b666567c34b319f5485ef60b6d0
Tolu Oyeniyi <tjoye20@gmail.com>
2016-10-23 20:16:16 -0500

    Update files.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Betterbeehive
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => :any, credentials: true
